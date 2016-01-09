da1a083ec4605a0ef16abad29fba2c9fd3a2f6a5
Robbert van Markus <robbert@vanmarkus.com>
2016-01-09 15:24:15 +0100

    grid component

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {NoUiSliderCmp}  from '../common/no-ui-slider.component';
+import {GridComponent} from '../common/grid.component';
 import {fromWebSocket} from 'rx-dom/dist/rx.dom';
 
 @Component({
   selector: 'home',
   directives: [NoUiSliderCmp],
-  template: `<no-ui-slider orientation="vertical" size="300" (change)="onSlideChange($event)"></no-ui-slider>`,
+  template: `<grid><no-ui-slider orientation="vertical" size="300" (change)="onSlideChange($event)"></no-ui-slider></grid>`,
   styleUrls: ['./components/home/home.css']
 })
 export class HomeCmp {
